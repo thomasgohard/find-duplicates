71a0f4e560afee3f442afcd0a3dd28770341eaff
Thomas Gohard <thomas@gohard-soutar.com>
2018-07-31 21:42:40 -0400

    Output the sets of duplicates found.

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ if (process.argv.length > 2) {
 
 // list all files in paths to analyse, get the file size and sha256 hash for each
 var files = {};
+var filesToProcess = 0;
+var filesProcessed = 0;
 paths.forEach(function(path) {
 	FS.readdir(path, function(error, listing) {
 		if (error) {
@@ -23,6 +25,8 @@ paths.forEach(function(path) {
 					if (error) {
 						console.error(error.name + ': ' + error.message);
 					} else if (stats.isFile()) {
+						++filesToProcess;
+
 						var fileSize = stats.size;
 
 						if (!files[fileSize]) {
@@ -44,6 +48,23 @@ paths.forEach(function(path) {
 								}
 
 								files[fileSize][hashDigest].push(filePath);
+
+								++filesProcessed;
+
+								if (filesProcessed == filesToProcess) {
+									for (var size in files) {
+										for (var digest in files[size]) {
+											if (files[size][digest].length > 1) {
+												console.log('Duplicates found:');
+												console.log('Hash: ' + digest);
+												console.log('Paths:');
+												for (path of files[size][digest]) {
+													console.log('\t' + path);
+												}
+											}
+										}
+									}
+								}
 							}
 						});
 					}
